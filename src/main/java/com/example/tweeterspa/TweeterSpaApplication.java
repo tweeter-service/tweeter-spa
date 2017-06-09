a5d7fb71c61df9ccab5c03a298d175d16d6e5fd1
making <makingx@gmail.com>
2017-06-09 17:21:31 +0900

    init

---
+++ src/main/java/com/example/tweeterspa/TweeterSpaApplication.java
@@ -0,0 +1,12 @@
+package com.example.tweeterspa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweeterSpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweeterSpaApplication.class, args);
+	}
+}
